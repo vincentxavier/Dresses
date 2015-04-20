1a22909ac495b6dc6b8e3af730d57a59e8d3a259
vincentxavier <endymion@thetys-retz.net>
2015-04-20 16:22:52 +0300

    Sélection rudimentaire des vêtements en fonction d'une température minimale
    L'affichage des vêtements se fait de façon sélective en fonction des vêtements.

--- ISN/dresses/views.py
+++ ISN/dresses/views.py
@@ -1,15 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import RequestContext, loader
 
 # Create your views here.
-
+from .models import Vetement
 from .meteo import get_meteo
 
 def index(request):
+    """Construction de l'index a partir des fonctions crees ailleurs"""
     temperature = get_meteo()
-    template = loader.get_template('dresses/index.html')
-    context = RequestContext(request, {
+    vetements = Vetement.objects.filter(temp_min__gte=10)
+    context = {
         'temperature': temperature,
-    })
-    return HttpResponse(template.render(context))
+        'vetements': vetements,
+    }
+    return render(request,"dresses/index.html",context)
