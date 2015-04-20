ac6a410a1ba6ee0d8a2ad4ebfe5d2cbba1d473dd
vincentxavier <endymion@thetys-retz.net>
2015-04-20 12:09:39 +0300

    Ajout de l'application "dresses"
    L'application sera présente à des fins de démonstrations

--- ISN/ISN/settings.py
+++ ISN/ISN/settings.py
@@ -66,9 +66,9 @@ DATABASES = {
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fr-fr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Paris'
 
 USE_I18N = True
 
